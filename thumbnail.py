750b818dbf2199977e95b97aa6f7f371ad3eaf42
Michael Spiegel <michael.m.spiegel@gmail.com>
2016-05-13 10:16:58 -0400

    Add progressive jpeg for thumbnails.
    People have differing options on progressive jpgs. My thoughts:
    on fast connections the thumbnails load so quickly it doesn't matter;
    on slow connections the low-res thumbnails look very similar to high-res.

--- thumbnail.py
+++ thumbnail.py
@@ -5,4 +5,4 @@ thumbDir = sys.argv[2]
 imageFiles = [f for f in os.listdir(imageDir) if os.path.isfile(os.path.join(imageDir, f))]
 for imageFile in imageFiles:
     if not os.path.exists(os.path.join(thumbDir, imageFile)):
-        os.system("convert " + os.path.join(imageDir, imageFile) + " -resize 65536@ " + os.path.join(thumbDir, imageFile))
+        os.system("convert " + os.path.join(imageDir, imageFile) + " -strip -interlace Plane -resize 65536@ " + os.path.join(thumbDir, imageFile))
